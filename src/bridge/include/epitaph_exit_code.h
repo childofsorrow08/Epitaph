68d02cef021b8c1be7040386ff9eff8b9a17fe38
Child of Sorrow <fogydark123@gmail.com>
2026-04-06 19:05:03 +0300

    🎨 Renamed a few things

--- src/bridge/include/epitaph_exit_code.h
+++ src/bridge/include/epitaph_exit_code.h
@@ -1,8 +1,8 @@
-#if !defined(EPITAPH_TEXT_EXIT_CODE)
-#define EPITAPH_TEXT_EXIT_CODE
+#if !defined(EPITAPH_EXIT_CODE)
+#define EPITAPH_EXIT_CODE
 
 typedef enum exit_code {
-    EPITAPH_TEXT_SUCCESS = 0, // Ended without problems
+    EPITAPH_SUCCESS = 0, // Ended without problems
 } exit_code;
 
 extern exit_code magic_numbers;
--- src/main.c
+++ src/main.c
@@ -6,9 +6,8 @@
 
 int main(int args, char *argv[]) {
 
-    magic_numbers = EPITAPH_TEXT_SUCCESS;
+    magic_numbers = EPITAPH_SUCCESS;
     
-    // TODO: Make compile options, to actually use this
     #if defined(EPITAPH_DEBUG)
         printf("[APP] [Info] Exiting with code %d \n", magic_numbers);
     #endif
